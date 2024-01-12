5bbadcee227be17dbfc767739305c554ec358283
Nikhil Dasar <kakashihatakesh6@gmail.com>
2024-01-12 13:42:05 +0530

    ned

--- pages/500.js
+++ pages/500.js
@@ -1,3 +1,7 @@
 export default function Custom500() {
-    return <h1>500 - Server-side error occurred</h1>
+    return (
+        <div className="flex min-h-screen w-[100%] justify-center items-center mt-4 p-4">
+            <h4>Server side error Occurred</h4>
+        </div>
+    )
 }
