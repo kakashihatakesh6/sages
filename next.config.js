572fd769220162778fdecf30fed6aa2f979ac20c
Nikhil Dasar <kakashihatakesh6@gmail.com>
2025-04-09 00:58:24 +0530

    feat: new homepage

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
+  // Exclude error pages from static generation
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    const paths = { ...defaultPathMap };
+    // Delete error pages from static export
+    delete paths['/404'];
+    delete paths['/500'];
+    return paths;
+  },
 }
 module.exports = nextConfig
